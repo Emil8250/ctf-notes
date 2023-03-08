cf30b7d29492cfb173f791fd5327ff5c785fa044
Emil Olesen <e-olesen@hotmail.com>
2023-03-08 20:01:25 +0100

    almost equal primes

--- crypto/rsa_solve.py
+++ crypto/rsa_solve.py
@@ -1,7 +1,7 @@
 from Crypto.Util.number import long_to_bytes
 import gmpy2
 import math
-
+from sympy import *
 
 #Solve simple RSA with known values
 def decrypt(p, q, e, ct):
@@ -18,3 +18,18 @@ def decrypt2(n, e, ct):
     d = pow(e, -1, phi)
     pt = pow(ct, d, n)
     return long_to_bytes(pt).decode()
+
+#Decrypting RSA with almost equal primes
+def decrypt3(n, e, ct):
+    base = int( gmpy2.iroot(n,2)[0])
+    p = base
+    q = base
+    while True:
+        p += 1
+        if isprime(p):
+            break
+    while True:
+        q -= 1
+        if isprime(q):
+            break
+    print(decrypt(p, q, e, ct))
